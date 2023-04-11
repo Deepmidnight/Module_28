e8d739f3177848a1e53b7dc87d7246a86f1856cb
Deepmidnight <munzer2006@yandex.ru>
2023-04-11 15:14:12 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'munzer2006@yandex.ru'
+    valid_password = 'Lolipop_08'
+    valid_email_reg = 'Ввести валидный email'
+    invalid_email = 'incorrect@mmail.ru'
+    invalid_password = 'inCOR4321'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Федя'
+    last_name = 'Сумкин'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'л'
+    latin_generate_string = 'incorrect'
+    chinese_chars = '形聲字象形字形声字/'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 845
+    empty = ''
+    passw1 = 'QwE1jf1'
+    passw2 = 'CvhgD54ds4'
+    passw3 = 'Fgh45JHnm767SV12'
